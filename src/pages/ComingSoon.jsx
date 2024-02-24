42db005ec59b6c2204c5e6043fc6980868400dcd
Tjerk Symens <98127222+tjerksymens@users.noreply.github.com>
2024-02-24 23:37:53 +0100

    fix: h1 css

--- src/pages/ComingSoon.jsx
+++ src/pages/ComingSoon.jsx
@@ -6,7 +6,7 @@ function ComingSoon() {
             <div className='heya2tone'>
                 <img src="heya2tone.svg" alt="Heya 2 tune" />
             </div>
-            <h1>Bringing a new vision to student housing</h1>
+            <h1 className='coming-soon-h1'>Bringing a new vision to student housing</h1>
         </div>
     )
 }
