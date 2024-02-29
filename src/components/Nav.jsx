26d3b9d49415702f2c083f23bc96b7243c305a9c
Tjerk Symens <98127222+tjerksymens@users.noreply.github.com>
2024-02-29 11:00:14 +0100

    fix: correct nav color on mobile

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -8,7 +8,8 @@ function Nav() {
     <nav className="nav">
         <Link to="/home" className={`nav__box nav__box__home ${location.pathname === "/home" ? "active" : ""}`}>
             <div className="nav__img-wrapper hidden__desktop">
-                <img src="../home.svg" alt="" />
+                <img className= {`do-not-show-on-desktop ${location.pathname === "/home" ? "hidden" : ""}`} src="../nav/home.svg" alt="" />
+                <img className= {`do-not-show-on-desktop ${location.pathname === "/home" ? "" : "hidden"}`} src="../nav/home-blue.svg" alt="" />
             </div>
             <img className={`hidden__mobile ${location.pathname === "/home" ? "" : "hidden"} heya__logo`} src="../heya-white.svg" alt="" />
             <img className={`hidden__mobile ${location.pathname === "/home" ? "hidden" : ""} heya__logo`} src="../heya-blue.svg" alt="" />
@@ -16,27 +17,31 @@ function Nav() {
         </Link>
         <Link to="/rooms" className={`nav__box nav__box__rooms ${location.pathname === "/rooms" ? "active" : ""}`}>
             <div className="nav__img-wrapper hidden__desktop">
-                <img src="../rooms.svg" alt="" />
+                <img className= {`do-not-show-on-desktop ${location.pathname === "/rooms" ? "hidden" : ""}`} src="../nav/rooms.svg" alt="" />
+                <img className= {`do-not-show-on-desktop ${location.pathname === "/rooms" ? "" : "hidden"}`} src="../nav/rooms-blue.svg" alt="" />
             </div>
             <span>Rooms</span>
         </Link>
         <Link to="/calendar" className={`nav__box nav__box__calendar ${location.pathname === "/calendar" ? "active" : ""}`}>
             <div className="nav__img-wrapper hidden__desktop">
-                <img src="../calendar.svg" alt="" />
+                <img className= {`do-not-show-on-desktop ${location.pathname === "/calendar" ? "hidden" : ""}`} src="../nav/calendar.svg" alt="" />
+                <img className= {`do-not-show-on-desktop ${location.pathname === "/calendar" ? "" : "hidden"}`} src="../nav/calendar-blue.svg" alt="" />
             </div>
             <span>Calendar</span>
         </Link>
         <Link to="/chat" className={`nav__box nav__box__chat ${location.pathname === "/chat" ? "active" : ""}`}>
             <div className="nav__img-wrapper hidden__desktop">
-                <img src="../chat.svg" alt="" />
+                <img className= {`do-not-show-on-desktop ${location.pathname === "/chat" ? "hidden" : ""}`} src="../nav/chat.svg" alt="" />
+                <img className= {`do-not-show-on-desktop ${location.pathname === "/chat" ? "" : "hidden"}`} src="../nav/chat-blue.svg" alt="" />
             </div>
             <span>Chat</span>
         </Link>
         <Link to="/profile" className={`nav__box nav__box__profile ${location.pathname === "/profile" ? "active" : ""}`}>
             <div className="nav__img-wrapper">
-                <img className="hidden__desktop" src="../profile.svg" alt="" />
-                <img className={`hidden__mobile ${location.pathname === "/profile" ? "hidden" : ""} heya__logo`} src="../profile-blue.svg" alt="" />
-                <img className={`hidden__mobile ${location.pathname === "/profile" ? "" : "hidden"} heya__logo`} src="../profile-beige.svg" alt="" />
+                <img className= {`do-not-show-on-desktop ${location.pathname === "/profile" ? "hidden" : ""}`} src="../nav/profile-mobile.svg" alt="" />
+                <img className= {`do-not-show-on-desktop ${location.pathname === "/profile" ? "" : "hidden"}`} src="../nav/profile-mobile-blue.svg" alt="" />
+                <img className={`hidden__mobile ${location.pathname === "/profile" ? "hidden" : ""} heya__logo`} src="../nav/profile-blue.svg" alt="" />
+                <img className={`hidden__mobile ${location.pathname === "/profile" ? "" : "hidden"} heya__logo`} src="../nav/profile-beige.svg" alt="" />
             </div>
             <span className="hidden__desktop">Profile</span>
         </Link>
