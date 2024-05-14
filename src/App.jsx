fac4c58f595149955024c4771eb921b0c20c9ad0
Tjerk Symens <98127222+tjerksymens@users.noreply.github.com>
2024-05-14 20:51:22 +0200

    feat: add create account style mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "antd/dist/antd.min.css";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from "./pages/Login";
+import CreateAccount from "./pages/CreateAccount.jsx";
 import ComingSoon from "./pages/ComingSoon";
 import Home from './pages/Home';
 import Rooms from './pages/Rooms';
@@ -16,6 +17,7 @@ function App() {
       <div>
         <Routes>
           <Route path="/login" element={<Login />} />
+          <Route path="/create-account" element={<CreateAccount />} />
           <Route path="/" element={<ComingSoon />} />
           <Route path="/home" element={<Home />} />
           <Route path="/rooms" element={<Rooms />} />
--- src/pages/CreateAccount.jsx
+++ src/pages/CreateAccount.jsx
@@ -1,4 +1,3 @@
-import '../css/login.css';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import RemoveRedEyeOutlinedIcon from '@mui/icons-material/RemoveRedEyeOutlined';
@@ -7,27 +6,41 @@ import IconButton from '@mui/material/IconButton';
 import GoogleIcon from '@mui/icons-material/Google';
 import FacebookRoundedIcon from '@mui/icons-material/FacebookRounded';
 
-function Login() {
+function CreateAccount() {
     const [showPassword, setShowPassword] = useState(false);
+    const [showConfirmPassword, setShowConfrimPassword] = useState(false);
     const navigate = useNavigate();
 
     const togglePasswordVisibility = () => {
         setShowPassword(!showPassword);
     };
 
-    const handleCreateAccount = () => {
-        navigate('/create-account');
+    const togglePasswordConfirmVisibility = () => {
+        setShowConfrimPassword(!showConfirmPassword);
     };
+
+    const handleLogin = () => {
+        navigate('/login');
+    };
+
 
     return (
         <div className="page__container">
             <img className="heya__logo__blue__round" src="../heya-blue-round.svg" alt="heya logo blue"/>
-            <h1 className="title__center">Login</h1>
+            <h1 className="title__center">Create Account</h1>
 
             <div className="form__group__email">
                 <input
                     type="email"
                     placeholder="Email"
+                    className="input__field"
+                />
+            </div>
+
+            <div className="form__group__name">
+                <input
+                    type="name"
+                    placeholder="Name"
                     className="input__field"
                 />
             </div>
@@ -45,18 +58,31 @@ function Login() {
                 >
                     {showPassword ? <VisibilityOffOutlinedIcon /> : <RemoveRedEyeOutlinedIcon />}
                 </IconButton>
-                <span className="forgot__password">Forgot password?</span>
+            </div>
+
+            <div className="form__group__confirm__password">
+                <input
+                    type={showConfirmPassword ? "text" : "password"}
+                    placeholder="Repeat password"
+                    className="input__field"
+                />
+                <IconButton
+                    onClick={togglePasswordConfirmVisibility}
+                    aria-label="toggle password visibility"
+                    className="password-icon"
+                >
+                    {showConfirmPassword ? <VisibilityOffOutlinedIcon /> : <RemoveRedEyeOutlinedIcon />}
+                </IconButton>
+                <span onClick={handleLogin} className="already__have__account">I already have an account</span>
             </div>
 
             <button className="google__button"><GoogleIcon fontSize="small" /> Create with Google</button>
             <button className="facebook__button"><FacebookRoundedIcon fontSize="small" /> Create with Facebook</button>
-
-            <button className="big__blue__button">Login</button>
+            <button className="big__blue__button">Create your account</button>
             <span className="or">Or</span>
-            <button onClick={handleCreateAccount} className="big__white__button">Create an account</button>
-
+            <button onClick={handleLogin} className="big__white__button">Login</button>
         </div>
     );
 }
 
-export default Login;
+export default CreateAccount;
