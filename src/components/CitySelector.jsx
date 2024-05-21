edf8daa6a770c81acd0977c700cbf7f767e0b582
wouter856 <wouterwaumans@live.be>
2024-05-21 19:08:11 +0200

    feat: static city selector

--- src/components/CitySelector.jsx
+++ src/components/CitySelector.jsx
@@ -6,22 +6,22 @@ const CitySelector = () => {
     <div className='city__select'>
         <h2>Popular cities</h2>
         <div className='city__select-cities'>
-            <div className='city__select-city'>
+            <div className='city__select-city image-Antwerp'>
                 <p className='strong__text'>Antwerp</p>
             </div>
-            <div className='city__select-city'>
+            <div className='city__select-city image-Leuven'>
                 <p className='strong__text'>Leuven</p>
             </div>
-            <div className='city__select-city'>
+            <div className='city__select-city image-Ghent'>
                 <p className='strong__text'>Ghent</p>
             </div>
-            <div className='city__select-city'>
+            <div className='city__select-city image-Brussels'>
                 <p className='strong__text'>Brussels</p>
             </div>
-            <div className='city__select-city'>
+            <div className='city__select-city image-Kortrijk'>
                 <p className='strong__text'>Kortrijk</p>
             </div>
-            <div className='city__select-city'>
+            <div className='city__select-city image-Hasselt'>
                 <p className='strong__text'>Hasselt</p>
             </div>
         </div>
