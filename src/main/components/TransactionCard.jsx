be3e741e017a8bb0a2d69521ddc53c1ebaae07bc
wouter856 <wouterwaumans@live.be>
2024-06-05 15:52:21 +0200

    feat: rooms housewoner setup + transaction cards + room cards

---
+++ src/main/components/TransactionCard.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import '../../shared/css/app.css';
+import '../../main/css/rooms.css';
+
+const imageMap = {
+    "Tjerk Symens": "../tjerk.webp",
+    "Wolf Ver Elst": "../wolf.webp",
+    "Wouter Waumans": "../wouter.webp"
+};
+
+function TransactionCard ({ transactionAmount, transactionDate, transactionTime, transactionUser, cardType }) {
+    const userImage = imageMap[transactionUser] || "../default.webp";
+
+    if (cardType === "small") {
+        return (
+            <div className='transaction__item small'>
+                <div className='transaction__main'>
+                    <img
+                        className="owner__image profile__image person__card--image--small"
+                        src={userImage}
+                        alt={transactionUser}
+                    />
+                    <div className='transaction__info'>
+                        <p className='transaction__name--small no__padding'>{transactionUser}</p>
+                        <p className='transaction__date--small no__padding'>{transactionDate}</p>
+                    </div>
+                </div>
+                <div className='transaction__price'>
+                    <h6 className='transaction__price__value--small no__padding'>{transactionAmount}</h6>
+                    <p className='transaction__time--small no__padding'>{transactionTime}</p>
+                </div>
+            </div>
+        );
+    } else if (cardType === "big") {
+        return (
+            <div className='transaction__item'>
+                <div className='transaction__main'>
+                    <img
+                        className="owner__image profile__image person__card--image"
+                        src={userImage}
+                        alt={transactionUser}
+                    />
+                    <div className='transaction__info'>
+                        <h4 className='transaction__name no__padding'>{transactionUser}</h4>
+                        <p className='transaction__date no__padding'>{transactionDate}</p>
+                    </div>
+                </div>
+                <div className='transaction__price'>
+                    <h3 className='transaction__price__value no__padding'>{transactionAmount}</h3>
+                    <p className='transaction__time no__padding'>{transactionTime}</p>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default TransactionCard;
--- src/main/pages/Rooms.jsx
+++ src/main/pages/Rooms.jsx
@@ -3,12 +3,12 @@ import { Navigate } from 'react-router-dom';
 import NavApp from '../components/NavApp.jsx';
 import Footer from '../../shared/components/Footer.jsx';
 import PersonCard from '../components/PersonCard.jsx';
+import RoomCard from '../components/RoomCard.jsx';
+import TransactionCard from '../components/TransactionCard.jsx';
 import '../css/rooms.css';
 import '../../shared/css/app.css';
 import AutoAwesomeRoundedIcon from '@mui/icons-material/AutoAwesomeRounded';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
 import SwapHorizontalCircleRoundedIcon from '@mui/icons-material/SwapHorizontalCircleRounded';
-import MapsHomeWorkRoundedIcon from '@mui/icons-material/MapsHomeWorkRounded';
 import FitnessCenterRoundedIcon from '@mui/icons-material/FitnessCenterRounded';
 import TvRoundedIcon from '@mui/icons-material/TvRounded';
 import DirectionsBikeRoundedIcon from '@mui/icons-material/DirectionsBikeRounded';
@@ -17,11 +17,7 @@ import RadioRoundedIcon from '@mui/icons-material/RadioRounded';
 import HotelRoundedIcon from '@mui/icons-material/HotelRounded';
 import ChairRoundedIcon from '@mui/icons-material/ChairRounded';
 import CottageRoundedIcon from '@mui/icons-material/CottageRounded';
-import ArrowBackIosNewRoundedIcon from '@mui/icons-material/ArrowBackIosNewRounded';
-import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
-import HandshakeRoundedIcon from '@mui/icons-material/HandshakeRounded';
-import CountertopsRoundedIcon from '@mui/icons-material/CountertopsRounded';
-import ShowerRoundedIcon from '@mui/icons-material/ShowerRounded';
+import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
 
 function Rooms() {
     const [isLoggedIn, setIsLoggedIn] = useState(true);
@@ -55,12 +51,24 @@ function Rooms() {
         window.location.href = '/contact';
     };
 
+    const changeThisRoom = () => {
+        //moet deze room editten
+        //nu tijdelijk console.log
+        console.log('Change this room');
+    };
+
+    const allRooms = () => {
+        //moet alle rooms laten zien
+        //nu tijdelijk console.log
+        console.log('All rooms');
+    };
+
 
     if (isLoggedIn) {
         //als de user ingelogd is gaan we kijken of die student is of houseowner
         //dit moet dus geslecteerd worden van het user profile of die student is of houseowner
-        const [Student, setStudent] = useState(true);
-        const [Houseowner, setHouseowner] = useState(false);
+        const [Student, setStudent] = useState(false);
+        const [Houseowner, setHouseowner] = useState(true);
 
         if (Student) {
             const [RoomRented, setRoomRented] = useState(true);
@@ -113,40 +121,8 @@ function Rooms() {
                                     <div className='room__transactions'>
                                         <h2 className='room__transactions__title'>Transactions</h2>
                                         <div className='transactions__list'>
-                                            <div className='transaction__item'>
-                                                <div className='transaction__main'>
-                                                    <img
-                                                        className="owner__image profile__image person__card--image"
-                                                        src="../tjerk.webp"
-                                                        alt="Tjerk Symens"
-                                                    />
-                                                    <div className='transaction__info'>
-                                                        <h4 className='transaction__name no__padding'>Tjerk Symens</h4>
-                                                        <p className='transaction__date no__padding'>June 2024</p>
-                                                    </div>
-                                                </div>
-                                                <div className='transaction__price'>
-                                                    <h3 className='transaction__price__value no__padding'>€34,50</h3>
-                                                    <p className='transaction__time no__padding'>2:42 PM</p>
-                                                </div>
-                                            </div>
-                                            <div className='transaction__item'>
-                                                <div className='transaction__main'>
-                                                    <img
-                                                        className="owner__image profile__image person__card--image"
-                                                        src="../wolf.webp"
-                                                        alt="Wolf Ver Elst"
-                                                    />
-                                                    <div className='transaction__info'>
-                                                        <h4 className='transaction__name no__padding'>Wolf Ver Elst</h4>
-                                                        <p className='transaction__date no__padding'>June 2024</p>
-                                                    </div>
-                                                </div>
-                                                <div className='transaction__price'>
-                                                    <h3 className='transaction__price__value no__padding'>€34,50</h3>
-                                                    <p className='transaction__time no__padding'>2:42 PM</p>
-                                                </div>
-                                            </div>
+                                            <TransactionCard transactionAmount="€32,50" transactionDate="June 2024" transactionTime="2:42 PM" transactionUser="Tjerk Symens" cardType="big" />
+                                            <TransactionCard transactionAmount="€34,50" transactionDate="May 2024" transactionTime="1:33 PM" transactionUser="Wolf Ver Elst" cardType="big" />
                                         </div>
                                         <div className='room__more-info'>
                                             <h2 className='room__transactions__title no__padding'>More information</h2>
@@ -320,13 +296,139 @@ function Rooms() {
         } else if (Houseowner) {
             return (
                 <div className="page__container">
-                    <NavApp />
-                    <div className="content">
-                        <h1>Your rooms</h1>
-                        <p>dit moet nog uitgewerkt worden</p>
+                        <NavApp />
+                        <div className="content student__with-room">
+                            <div className="room__basic-info">
+                                <div className="room__basic-info__text">
+                                    <h1 className='no__padding'>Your room</h1>
+                                    <div className="in-use">
+                                        <div className="in-use__circle"></div>
+                                        <p className="in-use__text">in use</p>
+                                    </div>
+                                </div>
+                                <div className="room__basic-info__cards">
+                                    <div className='basic-info__card basic-info__card--rating'>
+                                        <AutoAwesomeRoundedIcon sx={{ fontSize: 72 }} className='basic-info__card__icon rating__icon' />
+                                        <p className='basic-info__card__text rating__title'>Rate your room</p>
+                                    </div>
+                                    <div className='basic-info__card basic-info__card--unpaid'>
+                                        <p className='basic-info__card__text unpaid__title'>Unpaid rent: June</p>
+                                        <p className='basic-info__card__text unpaid__value'>€340</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="room__image__container">
+                                <div className="ellipse--image ellipse--room"></div>
+                                <div className="room--location--tag">
+                                    <div className="room--tag tag featured">
+                                        <p>Featured</p>
+                                    </div>
+                                    <p className="room--location">Lange Noordstraat 66</p>
+                                </div>
+                            </div>
+                            <div className='room__info'>
+                                <div className='room__info__main-content'>
+                                    
+                                </div>
+                                <div className='room__info__side-content'>
+                                    <div className='side-content--your-rooms'>
+                                        <h2 className='side-content__title no__padding'>Your rooms</h2>
+                                        <RoomCard location="Lange Noordstraat" houseowner="Tjerk Symens" />
+                                        <RoomCard location="Tolstraat" houseowner="Tjerk Symens" />
+                                        <button onClick={allRooms} className="white__button">
+                                            Open all rooms
+                                        </button>
+                                    </div>
+                                    <div className='side-content--overview'>
+                                        <div className="room__description--overview text__container">
+                                            <h4 className="room__description--overview__title text__container__title">Overview</h4>
+                                            <div className="room__description--overview__basics">
+                                                <div className="overview__basics--box">
+                                                    <HotelRoundedIcon fontSize="small" />
+                                                    <p className="overview__basics--text no__padding">Room, 1 person</p>
+                                                </div>
+                                                <div className="overview__basics--box">
+                                                    <SwapHorizontalCircleRoundedIcon fontSize="small" />
+                                                    <p className="overview__basics--text no__padding">12m² Personal space</p>
+                                                </div>
+                                                <div className="overview__basics--box">
+                                                    <ChairRoundedIcon fontSize="small" />
+                                                    <p className="overview__basics--text no__padding">Non-furnished</p>
+                                                </div>
+                                                <div className="overview__basics--box">
+                                                    <CottageRoundedIcon fontSize="small" />
+                                                    <p className="overview__basics--text no__padding">150m² Entire house</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className='room--icons--button'>
+                                            <button className='white__button'>Edit Details</button>
+                                            <div className="room--icons--edit">
+                                                <div className="room--icon">
+                                                    <FitnessCenterRoundedIcon />
+                                                    <p className="room--icon__description">Fitness</p>
+                                                </div>
+                                                <div className="room--icon">
+                                                    <TvRoundedIcon />
+                                                    <p className="room--icon__description">TV</p>
+                                                </div>
+                                                <div className="room--icon">
+                                                    <DirectionsBikeRoundedIcon />
+                                                    <p className="room--icon__description">Bike</p>
+                                                </div>
+                                                <div className="room--icon">
+                                                    <WifiRoundedIcon />
+                                                    <p className="room--icon__description">Wi-Fi</p>
+                                                </div>
+                                                <div className="room--icon">
+                                                    <RadioRoundedIcon />
+                                                    <p className="room--icon__description">Radio</p>
+                                                </div>
+                                                <div className="remaining--icons">
+                                                    <h6 className="remaining--icons__amount">+8</h6>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className='side-content--description'>
+                                        <div className="room__description--description text__container">
+                                            <h4 className="room__description--description__title text__container__title">
+                                                Description
+                                            </h4>
+                                            <p className="room__description--description__text no__padding">
+                                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+                                                Ipsum has been the industry's standard dummy text ever since the 1500s, when an
+                                                unknown printer
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <PersonCard userType="student" name="Wolf Ver Elst" />
+                                    <div className="rent__buttons">
+                                        <button onClick={changeThisRoom} className="blue__button">
+                                            Make changes to this room
+                                        </button>
+                                        <button onClick={reportProblem} className="white__button">
+                                            Report a problem
+                                        </button>
+                                        <a href="/contact" className='underline'>I need help</a>
+                                    </div>
+                                    <div className='side-content--transactions text__container'>
+                                        <div className='transactions__title'>
+                                            <h4 className='side-content--transactions__title text__container__title'>Transactions</h4>
+                                            <SearchRoundedIcon />
+                                        </div>
+                                        <div className='transactions__list'>
+                                            <TransactionCard transactionAmount="€32,50" transactionDate="June 2024" transactionTime="2:42 PM" transactionUser="Tjerk Symens" cardType="small" />
+                                            <TransactionCard transactionAmount="€34,50" transactionDate="May 2024" transactionTime="1:33 PM" transactionUser="Wolf Ver Elst" cardType="small" />
+                                            <TransactionCard transactionAmount="€32,50" transactionDate="May 2024" transactionTime="8:22 PM" transactionUser="Tjerk Symens" cardType="small" />
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <Footer />
                     </div>
-                    <Footer />
-                </div>
             );
         } else {
             return (
@@ -334,7 +436,11 @@ function Rooms() {
                     <NavApp />
                     <div className="content">
                         <h1>Error: No student or Houseowner</h1>
-                        <p>dit moet een error weergeven</p>
+                        <p>
+                            We seem to have an account error. <br />
+                            Please make sure your account is setup correctly on your <a className='underline' href="profile">profile page</a> <br />
+                            If the error still proceeds <a className='underline' href="contact">contact us</a>.
+                        </p>
                     </div>
                     <Footer />
                 </div>
