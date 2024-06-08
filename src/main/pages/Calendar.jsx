6dc75660a291a8b61bd62776b5808a44912dc5d8
Tjerk Symens <98127222+tjerksymens@users.noreply.github.com>
2024-06-08 16:41:37 +0200

    feat: add calendar

--- src/main/pages/Calendar.jsx
+++ src/main/pages/Calendar.jsx
@@ -1,12 +1,38 @@
+import React, { useState } from 'react';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { StaticDatePicker } from '@mui/x-date-pickers/StaticDatePicker';
+import { TextField, Typography } from '@mui/material';
 import NavApp from '../components/NavApp.jsx';
 import Footer from '../../shared/components/Footer.jsx';
+import '../css/calendar.css';
 
 function Calendar() {
+    const [selectedDate, setSelectedDate] = useState(new Date());
+
     return (
         <div className="page__container">
             <NavApp />
             <div className="content">
-                <h1>My Calendar</h1>
+                <h1>Your personal calendar</h1>
+                <div className={'calendarGrid'}>
+                    <div>
+                        <div className="calendar__container">
+                            <LocalizationProvider dateAdapter={AdapterDateFns}>
+                                <StaticDatePicker
+                                    displayStaticWrapperAs="desktop"
+                                    value={selectedDate}
+                                    onChange={(newValue) => {
+                                        setSelectedDate(newValue);
+                                    }}
+                                    renderInput={(params) => <TextField {...params} />}
+                                />
+                            </LocalizationProvider>
+                        </div>
+                        <div className={'daily-tasks-selected-date'}></div>
+                    </div>
+                    <div className={'next-three-days-selected-day'}></div>
+                </div>
             </div>
             <Footer />
         </div>
