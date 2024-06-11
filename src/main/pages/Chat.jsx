5a3d2e286241f0382cd40dd286c5195f636fffb1
Tjerk Symens <98127222+tjerksymens@users.noreply.github.com>
2024-06-11 11:39:24 +0200

    feat: laat de juiste afbeeldingen bij chat

--- src/main/pages/Chat.jsx
+++ src/main/pages/Chat.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import io from 'socket.io-client';
 import RecentChats from '../components/RecentChats';
 import NavApp from '../components/NavApp.jsx';
@@ -14,54 +14,43 @@ const socket = io(`${import.meta.env.VITE_API_URL}`, {
 
 function Chat() {
     const { otherUserId: urlOtherUserId } = useParams();
+    const navigate = useNavigate();
     const userId = localStorage.getItem('userId');
     const [messages, setMessages] = useState([]);
     const [messageInput, setMessageInput] = useState('');
     const [otherUserId, setOtherUserId] = useState(urlOtherUserId);
-    const [otherUserInfo, setOtherUserInfo] = useState({ firstName: 'User', lastName: '' });
+    const [otherUserInfo, setOtherUserInfo] = useState({ firstName: 'User', lastName: '', imageLink: '' });
+    const [isUserSelected, setIsUserSelected] = useState(!!urlOtherUserId);
+    const [userImage, setUserImage] = useState('');
     const messagesEndRef = useRef(null);
     const messagesContainerRef = useRef(null);
 
     useEffect(() => {
-        const fetchChats = async () => {
-            try {
-                let response;
-                if (otherUserId) {
-                    response = await fetch(`${import.meta.env.VITE_API_URL}/messages/all/${userId}/${otherUserId}`);
-                } else {
-                    response = await fetch(`${import.meta.env.VITE_API_URL}/messages/recent/${userId}`);
-                }
-                const data = await response.json();
-                setMessages(data);
-
-                if (data.length > 0) {
-                    const recentOtherUserId =
-                        otherUserId || (data[0].sender._id === userId ? data[0].receiver._id : data[0].sender._id);
-                    setOtherUserId(recentOtherUserId);
-                    await fetchUserInfo(recentOtherUserId, data);
-                }
-            } catch (error) {
-                console.error('Failed to fetch recent chats:', error);
-            }
-        };
-
-        const fetchUserInfo = async (id, messages) => {
+        const fetchUserInfo = async (id) => {
             try {
                 const userInfoResponse = await fetch(`${import.meta.env.VITE_API_URL}/users/${id}`);
                 const userInfoData = await userInfoResponse.json();
                 setOtherUserInfo(userInfoData);
             } catch (error) {
                 console.error('Failed to fetch user info:', error);
-                if (messages && messages.length > 0) {
-                    const recentMessage = messages[0];
-                    const fallbackUserInfo =
-                        recentMessage.sender._id === userId ? recentMessage.receiver : recentMessage.sender;
-                    setOtherUserInfo(fallbackUserInfo);
-                }
             }
         };
 
-        fetchChats();
+        const fetchUserImage = async () => {
+            try {
+                const userInfoResponse = await fetch(`${import.meta.env.VITE_API_URL}/users/${userId}`);
+                const userInfoData = await userInfoResponse.json();
+                setUserImage(userInfoData.imageLink);
+            } catch (error) {
+                console.error('Failed to fetch current user info:', error);
+            }
+        };
+
+        fetchUserImage();
+
+        if (urlOtherUserId) {
+            fetchUserInfo(urlOtherUserId);
+        }
 
         socket.on('receiveMessage', (msg) => {
             setMessages((prevMessages) => [...prevMessages, msg]);
@@ -71,7 +60,24 @@ function Chat() {
         return () => {
             socket.off('receiveMessage');
         };
-    }, [userId, otherUserId]);
+    }, [userId, urlOtherUserId]);
+
+    useEffect(() => {
+        if (otherUserId) {
+            const fetchMessages = async () => {
+                try {
+                    const response = await fetch(
+                        `${import.meta.env.VITE_API_URL}/messages/all/${userId}/${otherUserId}`,
+                    );
+                    const data = await response.json();
+                    setMessages(data);
+                } catch (error) {
+                    console.error('Failed to fetch chats:', error);
+                }
+            };
+            fetchMessages();
+        }
+    }, [otherUserId, userId]);
 
     useEffect(() => {
         scrollToBottom();
@@ -87,7 +93,7 @@ function Chat() {
             content: messageInput,
             timestamp: new Date().toISOString(),
             userId: userId,
-            sentToUserId: urlOtherUserId,
+            sentToUserId: otherUserId,
         };
         socket.emit('sendMessage', newMessage);
         setMessageInput('');
@@ -97,56 +103,94 @@ function Chat() {
         setMessageInput(event.target.value);
     };
 
+    const handleUserSelection = async (selectedUserId) => {
+        setIsUserSelected(true);
+        setOtherUserId(selectedUserId);
+        navigate(`/chat/${selectedUserId}`);
+        try {
+            const response = await fetch(`${import.meta.env.VITE_API_URL}/messages/all/${userId}/${selectedUserId}`);
+            const data = await response.json();
+            setMessages(data);
+            await fetchUserInfo(selectedUserId);
+        } catch (error) {
+            console.error('Failed to fetch chats:', error);
+        }
+    };
+
     return (
         <div className="page__container">
             <NavApp />
             <div className="content chat">
                 <div className="chat__content">
-                    <RecentChats />
+                    <RecentChats onSelectUser={handleUserSelection} />
                     <Divider orientation="vertical" variant="middle" />
                     <div className="chat--active">
-                        <div className="active--chat__header">
-                            <div className="active--chat__person">
-                                <div className="chat__lists--chat__image">
-                                    <img src="../tjerk.webp" alt="Tjerk Symens" />
+                        {isUserSelected ? (
+                            <>
+                                <div className="active--chat__header">
+                                    <div className="active--chat__person">
+                                        <div className="chat__lists--chat__image">
+                                            <img
+                                                src={otherUserInfo.imageLink || 'path/to/placeholder-image.png'}
+                                                alt="User"
+                                                className={!otherUserInfo.imageLink ? 'placeholder' : ''}
+                                            />
+                                        </div>
+                                        <h6 className="chat__info--name no__padding h6__strong">{`${otherUserInfo.firstName} ${otherUserInfo.lastName}`}</h6>
+                                    </div>
                                 </div>
-                                <h6 className="chat__info--name no__padding h6__strong">{`${otherUserInfo.firstName} ${otherUserInfo.lastName}`}</h6>
-                            </div>
-                        </div>
-                        <div className="messages chat__messages" ref={messagesContainerRef}>
-                            {messages.map((msg, index) => (
-                                <div key={index} className={`message ${msg.userId === userId ? 'sender' : 'receiver'}`}>
-                                    <h6 className="no__padding message__text">{msg.content}</h6>
-                                    <svg
-                                        className="message__balloon--svg"
-                                        width="20"
-                                        height="14"
-                                        viewBox="0 0 20 14"
-                                        fill="none"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                    >
-                                        <path
-                                            fillRule="evenodd"
-                                            clipRule="evenodd"
-                                            d="M15.0285 0H0C0 7.73199 6.26801 14 14 14H20V13.6709C18.5689 12.4697 17.3842 10.9871 16.5262 9.30324C15.4038 7.10032 15.107 4.39657 15.0285 0Z"
-                                        />
-                                    </svg>
-                                    <img className="message__image" src="../tjerk.webp" />
+                                <div className="messages chat__messages" ref={messagesContainerRef}>
+                                    {messages
+                                        .slice()
+                                        .reverse()
+                                        .map((msg, index) => (
+                                            <div
+                                                key={index}
+                                                className={`message ${msg.userId === userId ? 'sender' : 'receiver'}`}
+                                            >
+                                                <h6 className="no__padding message__text">{msg.content}</h6>
+                                                <svg
+                                                    className="message__balloon--svg"
+                                                    width="20"
+                                                    height="14"
+                                                    viewBox="0 0 20 14"
+                                                    fill="none"
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                >
+                                                    <path
+                                                        fillRule="evenodd"
+                                                        clipRule="evenodd"
+                                                        d="M15.0285 0H0C0 7.73199 6.26801 14 14 14H20V13.6709C18.5689 12.4697 17.3842 10.9871 16.5262 9.30324C15.4038 7.10032 15.107 4.39657 15.0285 0Z"
+                                                    />
+                                                </svg>
+                                                <img
+                                                    className={`message__image ${msg.userId === userId ? userImage : otherUserInfo.imageLink ? '' : 'placeholder'}`}
+                                                    src={
+                                                        msg.userId === userId
+                                                            ? userImage
+                                                            : otherUserInfo.imageLink || 'path/to/placeholder-image.png'
+                                                    }
+                                                    alt="User"
+                                                />
+                                            </div>
+                                        ))}
+                                    <div ref={messagesEndRef} />
                                 </div>
-                            ))}
-                            <div ref={messagesEndRef} />
-                        </div>
-                        <div className="input-box chat__input">
-                            <input
-                                type="text"
-                                value={messageInput}
-                                onChange={handleInputChange}
-                                placeholder="Type your message..."
-                            />
-                            <button onClick={sendMessage}>
-                                <SendRoundedIcon />
-                            </button>
-                        </div>
+                                <div className="input-box chat__input">
+                                    <input
+                                        type="text"
+                                        value={messageInput}
+                                        onChange={handleInputChange}
+                                        placeholder="Type your message..."
+                                    />
+                                    <button onClick={sendMessage}>
+                                        <SendRoundedIcon />
+                                    </button>
+                                </div>
+                            </>
+                        ) : (
+                            <div className="no-chat-selected">Please select a chat to start messaging</div>
+                        )}
                     </div>
                 </div>
             </div>
