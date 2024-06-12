14d0735c2e69e4a274563b0664600650715045a1
Wolf <wolfverelst@gmail.com>
2024-06-12 21:47:53 +0200

    style(homeowner register): main styling for PropertyDetails page

--- src/homeowner/pages/PropertyDetails.jsx
+++ src/homeowner/pages/PropertyDetails.jsx
@@ -8,6 +8,7 @@ import NavLogin from '../../shared/components/NavLogin.jsx';
 function PropertyDetails() {
     const navigate = useNavigate();
 
+
     const [selectedOptions, setSelectedOptions] = useState({
         activities: [],
         experiences: [],
@@ -35,11 +36,13 @@ function PropertyDetails() {
         <div className="page__container">
             <NavLogin />
             <div className="content">
-                <div className="center-container-always">
-                    <div className="accommodation__type__page">
+                <div className="center-container">
+                <div className="property__details__page">
+                <div className='homeowner__register__header'>
                         <h1>Tell us something more about</h1>
                         <h2>Your Property Details</h2>
-                        <div className="forGrid grid">
+                        </div>
+                        <div className="forGrid grid" style={{gridTemplateColumns:'1fr 1fr', marginTop: '40px'}}>
                         {['House', 'Appartement', 'Garden', 'No Garden'].map((option) => (
                             <div
                                 key={option}
@@ -50,32 +53,42 @@ function PropertyDetails() {
                             </div>
                         ))}
                     </div>
-                        
-                    <div className='flex'>
-                        <p>Surface area of the house/apartment in m²</p>
-                        <div className="inputWrapper">
-                            <input
-                                type="text"
-                                maxLength="4"
-                                placeholder="20"
-                                className={'input__field'}
-                            />
-                        </div>
-                        
-                        <p>Garden size in m²</p>
-                        <div className="inputWrapper">
-                            <input
-                                type="text"
-                                maxLength="4"
-                                placeholder="20"
-                                className={'input__field'}
-                            />
-                        </div>
-                    </div>
+                    
+                    <div className={'about__yourself__inputs'}>
+                                    
+                                        
+                                        <div className="form__group">
+                                        <p>Surface area of the house/apartment</p>
+                                        <div className="input-container">
+                                        <input
+                                            type="text"
+                                            placeholder="240"
+                                            className="input__field"
+                                            style={{ paddingRight: '30px' }}
+                                        />
+                                        <span className="input-unit" style={{ position: 'absolute', right: '10px', top: '50%', transform: 'translateY(-50%)' }}>m²</span>
+                                    </div>
+                                </div>                                        
+
+                                    <div className="form__group">
+                                        <p>Garden size</p>
+                                        <div className="input-container">
+                                        <input
+                                            type="text"
+                                            placeholder="240"
+                                            className="input__field"
+                                            style={{ paddingRight: '30px' }}
+                                        />
+                                        <span className="input-unit" style={{ position: 'absolute', right: '10px', top: '50%', transform: 'translateY(-50%)' }}>m²</span>
+                                    </div>
+                                </div>
+                                </div>
 
                     <div className="about__yourself__inputs">
                                     <div className="form__group">
-                                        <p>Total available rooms in the house/apartment</p><InfoOutlinedIcon />
+                                        <div className="title__with__icon">
+                                        <p>Total available rooms in the house/apartment</p><InfoOutlinedIcon style={{fontSize: '20'}} />
+                                        </div>
                                         <select className="input__field small">
                                             {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((num) => (
                                                 <option key={num} value={num}>{num}</option>
@@ -93,6 +106,7 @@ function PropertyDetails() {
                     </div>
                 </div>
             </div>
+            
             <Footer />
         </div>
     );
