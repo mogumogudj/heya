ec1ff3b3491a251e622eccf129bdc863eddc1558
Tjerk Symens <98127222+tjerksymens@users.noreply.github.com>
2024-06-16 22:49:12 +0200

    feat: fix pricing subtracting service cost

--- src/homeowner/pages/Pricing.jsx
+++ src/homeowner/pages/Pricing.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom'; // Import useLocation
 import Footer from '../../shared/components/Footer.jsx';
 import NavLogin from '../../shared/components/NavLogin.jsx';
 
 function Pricing() {
     const navigate = useNavigate();
+    const location = useLocation(); // Use useLocation hook to get location
 
     const [rent, setRent] = useState('');
     const [additionalCosts, setAdditionalCosts] = useState('');
@@ -21,13 +22,28 @@ function Pricing() {
         }
     }, [location]);
 
+    useEffect(() => {
+        if (rent !== '') {
+            const calculatedServiceCost = (parseFloat(rent) * 0.05).toFixed(2);
+            setServiceCost(calculatedServiceCost);
+        }
+    }, [rent]);
+
     const handleInputChange = (setFunction) => (event) => {
         setFunction(event.target.value);
     };
 
     const calculateTotal = () => {
-        const values = [rent, additionalCosts, serviceCost, deposit].map((value) => parseFloat(value) || 0);
-        return values.reduce((acc, value) => acc + value, 0).toFixed(2);
+        const rentValue = parseFloat(rent) || 0;
+        const additionalCostsValue = parseFloat(additionalCosts) || 0;
+        const serviceCostValue = parseFloat(serviceCost) || 0;
+        const depositValue = parseFloat(deposit) || 0;
+
+        let totalAmount = rentValue + additionalCostsValue - serviceCostValue + depositValue;
+        const depositFee = totalAmount * 0.02;
+        totalAmount -= depositFee;
+
+        return totalAmount.toFixed(2);
     };
 
     const handleNextStep = async () => {
@@ -68,19 +84,16 @@ function Pricing() {
             <div className="content" style={{ minHeight: '112vh' }}>
                 <div className="center-container" style={{ height: '100%' }}>
                     <div className="pricing__page">
-                        <div className='homeowner__register__header'>
-                        <h1>Set Your Room Pricing</h1>
-                        <h2>Enter the details below</h2>
+                        <div className="homeowner__register__header">
+                            <h1>Set Your Room Pricing</h1>
+                            <h2>Enter the details below</h2>
                         </div>
-                        
+
                         <div className="pricing__sections">
-
-
                             <div className="pricing__inputs">
                                 <div className="form__group">
                                     <p>Rent (/ month)</p>
                                     <div className="input-container">
-                                        
                                         <input
                                             type="text"
                                             maxLength="6"
@@ -88,27 +101,21 @@ function Pricing() {
                                             className={'input__field'}
                                             value={rent}
                                             onChange={handleInputChange(setRent)}
-                                            style={{ paddingRight: '30px' }}
+                                            style={{ paddingRight: '30px', color: 'green' }}
                                         />
                                         <span
-                                        className="input-unit"
-                                        style={{
-                                            position: 'absolute',
-                                            right: '10px',
-                                            top: '50%',
-                                            transform: 'translateY(-50%)',
-                                        }}
-                                    >
-                                        €
-                                    </span>
+                                            className="input-unit"
+                                            style={{
+                                                position: 'absolute',
+                                                right: '10px',
+                                                top: '50%',
+                                                transform: 'translateY(-50%)',
+                                            }}
+                                        >
+                                            €
+                                        </span>
                                     </div>
                                 </div>
-
-
-
-
-
-
 
                                 <div className="form__group">
                                     <p>Additional costs (/ month)</p>
@@ -120,22 +127,22 @@ function Pricing() {
                                             className={'input__field'}
                                             value={additionalCosts}
                                             onChange={handleInputChange(setAdditionalCosts)}
-                                            style={{ paddingRight: '30px' }}
+                                            style={{ paddingRight: '30px', color: 'green' }}
                                         />
                                         <span
-                                        className="input-unit"
-                                        style={{
-                                            position: 'absolute',
-                                            right: '10px',
-                                            top: '50%',
-                                            transform: 'translateY(-50%)',
-                                        }}
-                                    >
-                                        €
-                                    </span>
+                                            className="input-unit"
+                                            style={{
+                                                position: 'absolute',
+                                                right: '10px',
+                                                top: '50%',
+                                                transform: 'translateY(-50%)',
+                                            }}
+                                        >
+                                            €
+                                        </span>
                                     </div>
                                 </div>
-                                
+
                                 <div className="form__group">
                                     <p>
                                         Service cost <InfoOutlinedIcon />
@@ -148,23 +155,21 @@ function Pricing() {
                                             className={'input__field'}
                                             value={serviceCost}
                                             onChange={handleInputChange(setServiceCost)}
-                                            style={{ paddingRight: '30px' }}
+                                            style={{ paddingRight: '30px', color: 'red' }}
                                         />
                                         <span
-                                        className="input-unit"
-                                        style={{
-                                            position: 'absolute',
-                                            right: '10px',
-                                            top: '50%',
-                                            transform: 'translateY(-50%)',
-                                        }}
-                                    >
-                                        €
-                                    </span>
+                                            className="input-unit"
+                                            style={{
+                                                position: 'absolute',
+                                                right: '10px',
+                                                top: '50%',
+                                                transform: 'translateY(-50%)',
+                                            }}
+                                        >
+                                            €
+                                        </span>
                                     </div>
                                 </div>
-
-
 
                                 <div className="form__group">
                                     <p>
@@ -181,22 +186,20 @@ function Pricing() {
                                             style={{ paddingRight: '30px' }}
                                         />
                                         <span
-                                        className="input-unit"
-                                        style={{
-                                            position: 'absolute',
-                                            right: '10px',
-                                            top: '50%',
-                                            transform: 'translateY(-50%)',
-                                        }}
-                                    >
-                                        €
-                                    </span>
+                                            className="input-unit"
+                                            style={{
+                                                position: 'absolute',
+                                                right: '10px',
+                                                top: '50%',
+                                                transform: 'translateY(-50%)',
+                                            }}
+                                        >
+                                            €
+                                        </span>
                                     </div>
                                 </div>
                             </div>
 
-
-                            
                             <div className="pricing__summary">
                                 <div className="summary__header">
                                     <button className="white__button small">Help</button>
@@ -210,6 +213,7 @@ function Pricing() {
                                 <p>This can be changed later</p>
                             </div>
                         </div>
+
                         <div className="next__help">
                             <button className="blue__button medium" type="button" onClick={handleNextStep}>
                                 Next step
